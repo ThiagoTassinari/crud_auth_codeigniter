b84c55cc598bc26800fe9678a8b6f3b29d8af3d6
thiago_tassinari <thiagotassinari12@gmail.com>
2022-04-06 16:36:31 -0300

    Query for select of tb_games

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,9 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 	
+	
+	
 	public function index()
 	{
-
+		$this->load->model("games_model");
+		$data["games"] = $this->games_model->index();
         $data["title"] = 'Dashboard - CodeIgniter';
 		$this->load->view('pages/dashboard', $data);
 	}
