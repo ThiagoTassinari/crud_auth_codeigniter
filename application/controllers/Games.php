34063012049fb562e01d1073d8d883048b13bab2
thiago_tassinari <thiagotassinari12@gmail.com>
2022-04-07 10:16:50 -0300

    Views + Controllers of Dashborad and Games created

---
+++ application/controllers/Games.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Games extends CI_Controller {
+	
+	public function index()
+	{
+		$this->load->model("games_model");
+		$data["games"] = $this->games_model->index();
+        $data["title"] = 'Games - CodeIgniter';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav-top', $data);
+		$this->load->view('pages/games', $data);
+		$this->load->view('templates/footer', $data);
+		$this->load->view('templates/js', $data);
+	}
+
+	public function new()
+	{
+		$data["title"] = 'Games - CodeIgniter';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav-top', $data);
+		$this->load->view('pages/games/new', $data);
+		$this->load->view('templates/footer', $data);
+		$this->load->view('templates/js', $data);
+	}
+}
